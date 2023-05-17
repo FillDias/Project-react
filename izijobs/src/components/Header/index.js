3767499b602ea9369c6cbbed258ba2cce715992a
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2023-05-16 21:40:44 -0300

    feat/create-headerlp

---
+++ izijobs/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const HeaderLP = () => {
+  return (
+    <>
+      <header>
+        <div className='header'>
+          <img src='../assets/img/logo-izijobs.png' />
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default HeaderLP;
---
+++ izijobs/src/components/TextLP/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './TextLP.css'
+
+const TextLP = () => {
+  return (
+    <>
+      <div className='text__section'>
+        <h3>Sua nova trajetória profissional começa agora</h3>
+        <p>Acesse e candidate-se à milhares de vagas.</p>
+        <p>Entre e comece a se candidatar:</p>
+      </div>
+    </>
+  )
+}
+
+export default TextLP
